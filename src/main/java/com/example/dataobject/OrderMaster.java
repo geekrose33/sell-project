94fbf679e3eaaa6614f95751140413dfb8d6470c
joker-567 <62861464+joker-567@users.noreply.github.com>
2020-06-24 22:09:40 +0800

    jpa

---
+++ src/main/java/com/example/dataobject/OrderMaster.java
@@ -0,0 +1,34 @@
+package com.example.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author Joker_Dong
+ * @date 2020-6-24 11:43
+ */
+// 订单的主表 dataobject 中的
+// 类即是实体类 它是数据库的表字段---对应的类
+// 写成JavaBean模式 所有属性都是私有的 系统默认提供getset方法
+
+
+@Data
+// 等同于getter+setter
+@Entity
+// 实体类
+
+public class OrderMaster {
+    /*
+        订单Id
+    */
+    @Id
+    private String orderID;
+
+    /*
+        买家名字
+    */
+    private String buyerName;
+    
+}
